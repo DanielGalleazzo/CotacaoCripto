b67a87e925fbd51c0d799f8d1bcb47c0a3a39fcf
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2025-06-08 19:02:21 -0300

    Implementando novas coisas

--- CriptomoedasAPI/Program.cs
+++ CriptomoedasAPI/Program.cs
@@ -1,7 +1,8 @@
 ﻿using CriptomoedasAPI;
 
-Console.WriteLine("Insira o nome da moeda ( letra minúscula) ");
+Console.WriteLine("Insira o nome da moeda");
 string moedaNome = Console.ReadLine();
+Console.WriteLine("Todas as informações estão em USD");
 
 var moeda = await BuscarMoedas.BuscarMoedasAsync(moedaNome);
 
@@ -9,4 +10,8 @@ foreach (var Moeda in moeda)
 {
     Console.WriteLine("Nome da moeda: " + Moeda.Name);
     Console.WriteLine("Preço atual: " + Moeda.Current_price);
+    Console.WriteLine("Pico nas últimas 24 horas: " + Moeda.High_24h);
+    Console.WriteLine("Baixa das ultimás 24 horas: " + Moeda.Low_24h);
+    Console.WriteLine("Diferença da máxima e baixa ( 24 horas ): " + ( Moeda.High_24h - Moeda.Low_24h));
+    Console.WriteLine("Volume total: " + Moeda.Total_volume);
 }
